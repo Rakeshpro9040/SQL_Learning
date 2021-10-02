e8a611a513ae1e0a1072943a6c24c585c6ed5f6d
Rakeshpro9040 <57039842+Rakeshpro9040@users.noreply.github.com>
2021-10-02 09:17:42 +0530

    First SQLServerCommit

---
+++ SQLServerWorkspace/EmployeeDbQuery.sql
@@ -0,0 +1,11 @@
+/****** TOP 1000  ******/
+SELECT TOP (1000) [EmployeeID]
+      ,[FullName]
+      ,[EMPCode]
+      ,[Mobile]
+      ,[Position]
+  FROM [EmployeeDB].[dbo].[Employee];
+
+  /****** Select all  ******/
+  SELECT *
+  FROM [EmployeeDB].[dbo].[Employee];
